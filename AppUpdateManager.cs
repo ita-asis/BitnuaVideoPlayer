a4c8c48ac1945866989df78f09718615d3aa8349
Itamar Asis <ita.asis@gmail.com>
2018-08-18 14:06:08 +0300

    Squirrel update manager

---
+++ AppUpdateManager.cs
@@ -0,0 +1,127 @@
+﻿using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BitnuaVideoPlayer
+{
+    public class AppUpdateManager
+    {
+        private static AppUpdateManager s_instance;
+        private static object s_lock = new object();
+        private AppUpdateManager()
+        {
+            AppDomain.CurrentDomain.ProcessExit += (object sender, EventArgs e) => DisposeUpdateManager();
+
+            var t = checkForUpdates();
+        }
+
+        public static AppUpdateManager Instance
+        {
+            get
+            {
+                if (s_instance == null)
+                {
+                    lock (s_lock)
+                    {
+                        if (s_instance == null)
+                        {
+                            s_instance = new AppUpdateManager();
+                        }
+                    }
+                }
+
+                return s_instance;
+            }
+        }
+
+        private static async Task checkForUpdates()
+        {
+            try
+            {
+                using (s_UpdateManager = await getUpdateManager())
+                {
+                    var updateTask = s_UpdateManager.UpdateApp(OnVersionUpdateProgressChanged);
+                    var completedTask = updateTask.ContinueWith((e) =>
+                    {
+                        if (e.Result != null)
+                        {
+                            UpdateManager.RestartApp();
+                        }
+                    });
+                    await updateTask;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogException(ex);
+            }
+        }
+
+        private static async Task<Squirrel.UpdateManager> getUpdateManager()
+        {
+            return await UpdateManager.GitHubUpdateManager("https://github.com/ita-asis/BitnuaVideoPlayer");
+        }
+
+        private static int _isUpdateManagerDisposed = 1;
+        internal static void DisposeUpdateManager()
+        {
+            WaitForCheckForUpdateLockAcquire();
+
+            if (1 == Interlocked.Exchange(ref _isUpdateManagerDisposed, 0))
+            {
+                if (s_UpdateManager != null)
+                {
+                    s_UpdateManager.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Workaround for exception throw on SingleGlobalInstance destructor call.
+        /// Before app close we should wait for 2 seconds before SingleGlobalInstance will be disposed
+        /// </summary>
+        private static void WaitForCheckForUpdateLockAcquire()
+        {
+            var goTime = _lastUpdateCheckDateTime + TimeSpan.FromMilliseconds(2000);
+            var timeToWait = goTime - DateTime.Now;
+            if (timeToWait > TimeSpan.Zero)
+            {
+                Task.Delay(timeToWait).Wait();
+            }
+        }
+        private static DateTime _lastUpdateCheckDateTime = DateTime.Now - TimeSpan.FromDays(1);
+        private static UpdateManager s_UpdateManager;
+
+        private static async Task<UpdateInfo> CheckForUpdate(bool ignoreDeltaUpdates)
+        {
+            _lastUpdateCheckDateTime = DateTime.Now;
+            return await s_UpdateManager.CheckForUpdate(ignoreDeltaUpdates);
+        }
+
+
+        public static event Action<int> VersionUpdateProgressChanged;
+        public static void OnVersionUpdateProgressChanged(int progress)
+        {
+            if (VersionUpdateProgressChanged != null)
+            {
+                VersionUpdateProgressChanged.Invoke(progress);
+            }
+        }
+
+        internal static void LogException(Exception exception)
+        {
+            var ex = exception;
+            while (ex != null)
+            {
+                Trace.WriteLine(ex.Message);
+                Trace.WriteLine(ex.StackTrace);
+                ex = ex.InnerException;
+            }
+        }
+    }
+}
