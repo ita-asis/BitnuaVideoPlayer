ea6efbaf95c9d42353941e0dce36786efdc1373e
Itamar Asis <ita.asis@gmail.com>
2021-08-13 22:47:07 +0300

    BitmapImageConverter - file existance check

--- UI/Converters/BitmapImageConverter.cs
+++ UI/Converters/BitmapImageConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,9 @@ namespace BitnuaVideoPlayer
         {
             string path = value?.ToString();
             if (string.IsNullOrEmpty(path))
+                return null;
+
+            if (!File.Exists(path))
                 return null;
 
             // Create source.
